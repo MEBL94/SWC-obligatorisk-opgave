9b3254c2cf9c9d1e94b18b7558c422883549afa2
mebl94 <mebl94@gmail.com>
2017-10-03 13:43:59 +0200

    more basic structure

--- src/main/java/dk/mebl/controller/HomeController.java
+++ src/main/java/dk/mebl/controller/HomeController.java
@@ -3,8 +3,16 @@ package dk.mebl.controller;
 import dk.mebl.BL.IUserRepo;
 import dk.mebl.BL.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
     @Autowired
     IUserRepo userRepo = new UserRepo();
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
