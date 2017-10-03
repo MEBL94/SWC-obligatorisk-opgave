b392ce08e9cba206bd2f4e56169702df3b01befd
mebl94 <mebl94@gmail.com>
2017-10-03 11:34:01 +0200

    Initial commit

---
+++ src/main/java/dk/mebl/ObligatoriskOpgaveApplication.java
@@ -0,0 +1,12 @@
+package dk.mebl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoriskOpgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatoriskOpgaveApplication.class, args);
+	}
+}
